bad37eae16f0a13a6606a653ae4a6d0610035af4
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-03-27 15:26:38 -0300

    Added Directives & Template form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faChevronCircleDown, faChevronCircleUp } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { LikeComponent } from './like/like.component';
+import { ZippyComponent } from './zippy/zippy.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorsComponent,
     TitleCasePipe,
-    LikeComponent
+    LikeComponent,
+    ZippyComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +32,6 @@ import { LikeComponent } from './like/like.component';
 export class AppModule {
   constructor(library: FaIconLibrary) {
     // Add an icon to the library for convenient access in other components
-    library.addIcons(faHeart);
+    library.addIcons(faHeart, faChevronCircleDown, faChevronCircleUp);
   }
  }
